6d7d6f36e70a1bc7e95ec828113aea7e15307a0c
ansasa2122daw2 <ansasa2122daw2@protonmail.com>
2022-01-22 16:39:05 +0100

    commit random

--- JS.js
+++ JS.js
@@ -1,10 +1,28 @@
-var canvas  = document.getElementById('Canvas'),
-ctx = canvas.getContext('2d');
-    
-function draw() {
-  var canvas = document.getElementById('canvas');
-  var context = canvas.getContext('2d');
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
   
 let image = document.getElementById('player1');
-context.drawImage(image, 0,0);
+context.drawImage(image, 80, 180);
+
+var imageData = context.getImageData( 80, 180, canvas.width, canvas.height);
+var pixels = imageData.data
+
+window.addEventListener("gamepadconnected", function() {
+
+  player1 = document.getElementById("player1");
+  updateLoop();
+
+});
+
+function updateLoop() {
+  var gp = navigator.getGamepads()[0];
+  var left = (gp.axes[0] + 1) / 2 * (window.innerWidth - pixels.offsetWidth);
+  var right = (gp.axes[1] + 1) / 2 * (window.innerHeight - pixels.offsetHeight);
+
+  pixels.style.left = left + "px";
+  pixels.style.top =  right + "px";
+
+
+  requestAnimationFrame(updateLoop);
+
 }
