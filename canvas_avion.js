f04a488f555de5b4a0911b9843564d7cfcf1c184
ansasa2122daw2 <ansasa2122daw2@protonmail.com>
2022-01-24 17:28:01 +0100

    Commit avion + fondo

--- canvas_avion.js
+++ canvas_avion.js
@@ -3,8 +3,16 @@ window.onload = function () {
 	let ctx = canvas.getContext("2d");
 	let img = new Image();
 	img.src = "./images/nave.png";
+	let fondo = new Image();
+	fondo.src = "./images/dark-paths.png";
+
+	/* enemigo*/
+	let enemigo = new Image();
+	enemigo.src = "./images/enemigo.png";
 	img.onload = function () {
-		ctx.drawImage(img, 0, 0, 200, 200);
+		ctx.drawImage(fondo, 0, 0, ctx.canvas.width, ctx.canvas.height);
+		ctx.drawImage(img, 0, 0, 130, 130);
+		ctx.drawImage(enemigo, 0, 30, 60, 60);
 	};
 	var imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
 
@@ -17,19 +25,35 @@ window.onload = function () {
 		xAxis = Math.floor(gp.axes[0]) + xAxis;
 		yAxis = Math.floor(gp.axes[1]) + yAxis;
 		if (xAxis !== 0 || yAxis !== 0) {
-			// var imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
-			// var xAxis = ((gp.axes[0] + 1) / 2) * (canvas.width - pixels.offsetWidth);
-			// var yAxis = ((gp.axes[1] + 1) / 2) * (canvas.width - pixels.offsetHeight);
-
-			// pixels["style"].left = left + "px";
-			// pixels["style"].top = right + "px";
 			console.log("xAxis", xAxis);
 			console.log("yAxis", yAxis);
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			ctx.drawImage(img, xAxis, yAxis, 200, 200);
+			ctx.drawImage(fondo, 0, 0, ctx.canvas.width, ctx.canvas.height);
+			ctx.drawImage(img, xAxis, yAxis, 130, 130);
+			if (xAxis < -30) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.drawImage(fondo, 0, 0, ctx.canvas.width, ctx.canvas.height);
+				ctx.drawImage(img, xAxis + 520, yAxis, 130, 130);
+			}
 		}
 		requestAnimationFrame(animate);
 	}
+
+	// (moveenemigo) => {
+	// 	for (i = 0; i < 5; i++) {
+	// 		ctx.drawImage(enemigo, 70, 0, 60, 60);
+	// 	}
+	// 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	// 	ctx.drawImage(enemigo, 0, 0, 60, 60);
+	// 	let x = 0;
+	// 	let y = 0;
+	// 	if (y >= 30) {
+	// 		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	// 		ctx.drawImage(fondo, 0, 0, ctx.canvas.width, ctx.canvas.height);
+	// 		ctx.drawImage(img, x, y + 30, 130, 130);
+	// 	}
+	// 	requestAnimationFrame(moveenemigo);
+	// };
 
 	window.addEventListener("gamepadconnected", function (e) {
 		console.log("GAMEPAD CONNECTED");
